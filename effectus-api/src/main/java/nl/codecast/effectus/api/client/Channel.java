a53a410aeffd346ab578567ff18fa9e5fb44c9a1
Bas Verburg <b.verburg@codecast.nl>
2013-04-24 22:39:18 +0200

    api work

---
+++ effectus-api/src/main/java/nl/codecast/effectus/api/client/Channel.java
@@ -0,0 +1,7 @@
+package nl.codecast.effectus.api.client;
+
+public interface Channel {
+
+	public void open();
+	public void close();	
+}
--- effectus-api/src/main/java/nl/codecast/effectus/api/client/Request.java
+++ effectus-api/src/main/java/nl/codecast/effectus/api/client/Request.java
@@ -2,4 +2,5 @@ package nl.codecast.effectus.api.client;
 
 public interface Request {
 
+	public Message getMessage();
 }
